3d22056b534ddfbe1fa35f4795088ec0f543a9bd
danielsouzzz <danielsouzanovo@gmial.com>
2024-08-06 20:09:37 -0300

    adicionado arquivo Readme para orientar a utilizacao do scrip monitoramento_processes.sh

--- monitoramento_processes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-output_file="/home/daniel/Estudos/cursoDevOps/top_processes_$(date +\%Y\%m\%d_\%H\%M).txt"
-
-# Adiciona cabeçalho manualmente
-echo "PID %MEM USER COMMAND" > "$output_file"
-
-# Coleta informações dos processos e adiciona ao arquivo
-ps -e -o pid,%mem,user,comm --sort=-%mem | head -n 11 >> "$output_file"
